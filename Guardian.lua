0eb9b1a082a7b4b407af756b6d57862160c8144c
Glowahh <120524439+Glowahh@users.noreply.github.com>
2023-03-14 18:58:26 -0500

    Update Guardian.lua

--- Guardian.lua
+++ Guardian.lua
@@ -46,6 +46,18 @@ getgenv().Guardian={
             end
         end
     end
+}
+--game:GetService("StarterGui"):SetCore("SendNotification",{
+  --  Title="Guardian has been activated",
+  --  Text="Guardian is now protecting you in the background",
+  --  Button1="Okay!",
+  --  Duration=0
+})
+--game:GetService("StarterGui"):SetCore("SendNotification",{
+ --   Title="By Hypernova#0721",
+ --   Text="With love <3",
+  --  Button1="<3",
+  --  Duration=0
 })
 if not hookmetamethod or not hookfunction then
     task.spawn(function()error("Your executor does not support a core function needed for this script. Expect detections and poor security")end)
